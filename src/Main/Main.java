0f000d662d3f73b26644c7bfaf6772847e5c397b
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-21 10:52:02 +0700

    Add files via upload
    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Main {
+    
+    //memberi nilai ke variable
+    
+    public static void main(String[] args) {
+        //Deklarasi variable
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        // TODO code application logic here
+        nilaiInt = 78;    
+        nilaiLogika =  false;
+        nilaiKarakter = 'D';
+        System.out.println();
+        System.out.println("Isi variable nilai = "+nilaiInt);
+        System.out.println("Isi variable PHI = "+PHI);
+        System.out.println("Isi variable Logika = "+nilaiLogika);
+        System.out.println("Isi variable Karakter = "+nilaiKarakter);
+
+    }
+}
